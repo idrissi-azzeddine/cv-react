277475cef8c187fd72f8704e30ddf9141121bcc0
AZZEDDINE IDRISSI <116524585+idrissi-azzeddine@users.noreply.github.com>
2023-01-31 01:27:52 +0100

    Update Sect.jsx

--- src/Sect.jsx
+++ src/Sect.jsx
@@ -134,7 +134,7 @@ $(document).ready(function () {
         $('.aq').hide();
         setTimeout(function() {
              navigation('/cv')
-           }, 3000); 
+           }, 2000); 
     })
     $('.zwei').keydown(function () { 
         $('.zwei').show();
